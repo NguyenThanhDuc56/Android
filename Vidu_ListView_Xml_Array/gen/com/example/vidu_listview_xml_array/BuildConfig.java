cc928bdacc031a761fa5120a3db1a8211fc518ac
Nguyen Duc <nnnguyen.duc@gmail.com>
2017-05-16 21:16:59 +0700

    Revert "Add files via upload"
    This reverts commit 30629ec30db23da7dcf069639eeb4cea9b43d32d.

--- Vidu_ListView_Xml_Array/gen/com/example/vidu_listview_xml_array/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.vidu_listview_xml_array;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
